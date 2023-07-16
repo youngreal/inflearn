8ad28450f6e2fbad59a7c81e80610c913ba7ccc5
youngreal <59333182+youngreal@users.noreply.github.com>
2023-07-16 18:31:27 +0900

    refactor: 회원가입후 로그인되는부분 변경
    서비스에서 서비스 메서드를 호출하는것보다는 컨트롤러가 다른서비스를 호출하는게 적합하다고 판단해서 리팩토링했다.

--- src/main/java/com/example/musinsa/ui/member/MemberRestController.java
+++ src/main/java/com/example/musinsa/ui/member/MemberRestController.java
@@ -37,11 +37,22 @@ public class MemberRestController {
     //todo 프론트에서 RequestParam으로 받는게 편할까? DTO로 받는게 편할까?
 
     @GetMapping("/check-email-token")
-    public void checkEmail(
+    public ResponseEntity<Void> checkEmail(
             @RequestParam @NotBlank String emailToken,
             @RequestParam @NotBlank @Email String email
     ) {
-        memberService.emailCheck(emailToken, email);
+        Member member = memberService.emailCheck(emailToken, email);
+        String sessionToken = memberService.login(member);
+
+        ResponseCookie cookie = ResponseCookie.from("SESSION", sessionToken)
+                .domain("localhost") //todo 개발할땐 localhost지만 개발,운영에서는 변경될수있다. (profile 설정값으로 설정파일에서 추가하면 좋을것이다)
+                .httpOnly(true)
+                .secure(true)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
     }
 
     @PostMapping("/login")
