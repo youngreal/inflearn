2276722841420a1bbbf7b30bc774304de47cab8a
youngreal <59333182+youngreal@users.noreply.github.com>
2023-07-18 01:36:44 +0900

    feat: #11 - Post Domain
    * 연관관계만 필요한상태로 걸어주고 외래키제약조건은 설정해두지않았다.
    * 요구사항의 잦은변경으로 인해 엔티티가 계속 변경될것이므로 걸지않는게 의미있다고 생각했다.

---
+++ src/main/java/com/example/musinsa/domain/Comment.java
@@ -0,0 +1,22 @@
+package com.example.musinsa.domain;
+
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Post post;
+}
