9ca5828cb4d4895e813758f46ce64e8f42b74b50
youngreal <59333182+youngreal@users.noreply.github.com>
2023-08-21 15:46:55 +0900

    refactor: membeService에서 Mail의존성 줄이기
    - 이벤트가 추가되면 메일 메시지 생성, 메일전송 로직을 MailSentEvent에서 하게끔 변경(관심사 분리)

--- src/main/java/com/example/musinsa/event/MailSentEvent.java
+++ src/main/java/com/example/musinsa/event/MailSentEvent.java
@@ -12,4 +12,14 @@ public class MailSentEvent{
     public MailSentEvent(Member member) {
         this.message = emailMessage(member);
     }
+
+    private EmailMessage emailMessage(Member member) {
+        return EmailMessage.builder()
+                .to(member.getEmail())
+                .subject("[인프런] 회원가입을 위해 메일인증을 해주세요.")
+                .message("안녕하세요, 인프랩입니다. 아래 메일 인증 버튼을 눌러 회원가입을 완료해주세요.\n"
+                        + "/check-email-token?emailToken=" + member.getEmailToken() +
+                        "&email=" + member.getEmail())
+                .build();
+    }
 }
