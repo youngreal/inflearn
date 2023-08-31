33542966fa2875a059cb427b92739d1d968b5a98
youngreal <59333182+youngreal@users.noreply.github.com>
2023-08-31 15:21:05 +0900

    feat: 이벤트 retry, recover 적용

--- src/main/java/com/example/musinsa/App.java
+++ src/main/java/com/example/musinsa/App.java
@@ -2,7 +2,11 @@ package com.example.musinsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableRetry // Spring Retry활성화를 위해
+@EnableAsync
 @SpringBootApplication
 public class App {
 
