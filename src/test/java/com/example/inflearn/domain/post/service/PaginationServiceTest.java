4493b78a0e0f8ca8eca6165c745eced8c8e18740
youngreal <59333182+youngreal@users.noreply.github.com>
2023-09-29 19:58:53 +0900

    test: 게시글검색 & 페이지네이션 테스트
    * 쿼리 테스트는 생략, 실제 API를 호출해보는 테스트로 대체한다.

--- src/test/java/com/example/inflearn/domain/post/service/PaginationServiceTest.java
+++ src/test/java/com/example/inflearn/domain/post/service/PaginationServiceTest.java
@@ -3,48 +3,82 @@ package com.example.inflearn.domain.post.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import java.util.List;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class PaginationServiceTest {
 
-    @InjectMocks
     private PaginationService sut;
 
-    @DisplayName("페이지번호와 전체 페이지수를 입력받고 페이지에 표시될 페이지번호를 반환한다")
-    @MethodSource
-    @ParameterizedTest(name = "[{index}] 현재 페이지: {0}, 총 페이지: {1} => {2}")
-    void 페이지에_표시될_페이지번호를_반환한다(int pageNumber, int totalPage, List<Integer> expected) {
-        // given
+    @BeforeEach
+    void setup() {
+        sut = new PaginationService();
+    }
 
-        // when
-        List<Integer> actual = sut.getPageNumbers(pageNumber, totalPage);
+    @DisplayName("게시글 검색을 위한 offSet을 계산한다")
+    @MethodSource
+    @ParameterizedTest
+    void offset_when_searchPost(int input, int expected) {
+        // given& when
+        int actual = sut.offSetWhenSearchPost(input);
 
         // then
         assertThat(actual).isEqualTo(expected);
     }
 
-    static Stream<Arguments> 페이지에_표시될_페이지번호를_반환한다() {
+    static Stream<Arguments> offset_when_searchPost() {
         return Stream.of(
-                arguments(1, 13, List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)),
-                arguments(2, 13, List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)),
-                arguments(3, 13, List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)),
-                arguments(4, 13, List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)),
-                arguments(5, 50, List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)),
-                arguments(6, 0, List.of(1)),
-                arguments(10, 13, List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)),
-                arguments(11, 13, List.of(1, 1, 11, 12, 13)),
-                arguments(12, 50, List.of(1, 1, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21)),
-                arguments(21, 50, List.of(1, 11, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31))
+                arguments(1,0),
+                arguments(5,80),
+                arguments(10,180),
+                arguments(11,200),
+                arguments(15,280)
         );
     }
 
+    @DisplayName("페이지번호를 구하기위한 size를 계산한다")
+    @Test
+    void size_when_getPageNumbers() {
+        // given
+        int size = 20;
+
+        // when
+        int actual = sut.sizeWhenGetPageNumbers(size);
+
+        // then
+        assertThat(actual).isEqualTo(200);
+    }
+
+
+    @DisplayName("페이지번호를 구하기위한 offset을 계산한다")
+    @MethodSource
+    @ParameterizedTest
+    void offset_when_getPageNumbers(int input, int expected) {
+        // given & when
+        int actual = sut.offsetWhenGetPageNumbers(input);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+    static Stream<Arguments> offset_when_getPageNumbers() {
+        return Stream.of(
+                arguments(3,0),
+                arguments(1,0),
+                arguments(5,0),
+                arguments(10,0),
+                arguments(11,200),
+                arguments(20,200),
+                arguments(21,400),
+                arguments(30,400),
+                arguments(100,1800),
+                arguments(101,2000),
+                arguments(102,2000),
+                arguments(105,2000)
+        );
+    }
 }
--- src/test/java/com/example/inflearn/domain/post/service/PostQueryServiceTest.java
+++ src/test/java/com/example/inflearn/domain/post/service/PostQueryServiceTest.java
@@ -6,25 +6,27 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
 import com.example.inflearn.common.exception.DoesNotExistPostException;
-import com.example.inflearn.domain.post.domain.Post;
+import com.example.inflearn.dto.PostDto;
 import com.example.inflearn.infra.repository.post.PostRepository;
+import com.example.inflearn.ui.post.dto.request.PostSearch;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 @ExtendWith(MockitoExtension.class)
 class PostQueryServiceTest {
 
     @InjectMocks
     private PostQueryService sut;
+
+    @Mock
+    private PaginationService paginationService;
 
     @Mock
     private PostRepository postRepository;
@@ -34,17 +36,17 @@ class PostQueryServiceTest {
     void 게시글_상세정보를_조회한다 () {
         // given
         long postId = 1;
-        Post post = Post.builder()
+        PostDto postDto = PostDto.builder()
                 .title("글제목")
                 .contents("글내용")
                 .build();
-        given(postRepository.findById(postId)).willReturn(Optional.of(post));
+        given(postRepository.findById(postId)).willReturn(Optional.of(postDto.toEntity()));
 
         // when
-        Post actual = sut.postDetail(postId);
+        PostDto actual = sut.postDetail(postId);
 
         // then
-        assertThat(actual).isEqualTo(post);
+        assertThat(actual).isEqualTo(postDto);
     }
 
     @DisplayName("게시글 상세정보 조회 실패 : 게시글이 존재하지 않는다")
@@ -58,34 +60,45 @@ class PostQueryServiceTest {
         assertThrows(DoesNotExistPostException.class, () -> sut.postDetail(postId));
     }
 
-    @DisplayName("검색어 없이 게시글을 검색하면 비어있는 게시글 목록을 반환한다")
-    @Test
-    void 검색어_없이_게시글을_조회하면_비어있는_게시글_목록을_반환한다() {
-        // given
-        String searchWord = "";
-        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Order.desc("createdAt")));
-
-        // when
-        List<Post> actual = sut.searchPost(searchWord, pageable);
-
-        // then
-        assertThat(actual).isEmpty();
-        then(postRepository).shouldHaveNoInteractions();
-    }
-
     @DisplayName("검색어를 포함해 게시글을 검색하면 해당 검색어를 포함하고있는 게시글들을 반환한다")
     @Test
     void 검색어로_게시글을_검색하면_검색어를_포함하고있는_게시글목록을_반환한다() {
         // given
-        String searchWord = "자바";
-        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Order.desc("createdAt")));
-        given(postRepository.findByTitleContainingOrContentsContaining(searchWord, searchWord, pageable)).willReturn(List.of());
+        PostSearch postSearch = new PostSearch(1, 20, "자바");
+        PostDto postDto = PostDto.builder()
+                .title("자바스터디1")
+                .contents("자바스터디구합니다")
+                .hashtags(Set.of())
+                .build();
 
         // when
-        List<Post> actual = sut.searchPost(searchWord, pageable);
+        given(paginationService.offSetWhenSearchPost(postSearch.page())).willReturn(0);
+        given(postRepository.search(postSearch.searchWord(),paginationService.offSetWhenSearchPost(postSearch.page()), postSearch.size())).willReturn(List.of(postDto.toEntity()));
+
+        // when
+        List<PostDto> actual = sut.searchPost(postSearch.searchWord(), postSearch.page(), postSearch.size());
 
         // then
-        assertThat(actual).isEmpty();
-        then(postRepository).should().findByTitleContainingOrContentsContaining(searchWord, searchWord, pageable);
+        assertThat(actual).isEqualTo(List.of(postDto));
+        assertThat(actual).hasSize(1);
+    }
+
+    @DisplayName("검색어를 포함해 게시글을 검색하면 해당 페이지에 표시될 페이지 개수를 계산할수있도록 total을 전해준다")
+    @Test
+    void page_count_success() {
+        // given
+        PostSearch postSearch = new PostSearch(1, 20, "자바");
+
+        // when
+        given(paginationService.offsetWhenGetPageNumbers(postSearch.page())).willReturn(0);
+        given(paginationService.sizeWhenGetPageNumbers(postSearch.size())).willReturn(postSearch.size() * 10);
+        given(postRepository.countPage(postSearch.searchWord(),paginationService.offsetWhenGetPageNumbers(postSearch.page()), paginationService.sizeWhenGetPageNumbers(postSearch.size()))).willReturn(1L);
+
+        // when
+        Long actual = sut.getPageCount(postSearch.searchWord(), postSearch.page(), postSearch.size());
+
+        // then
+        then(postRepository).should().countPage(postSearch.searchWord(), paginationService.offsetWhenGetPageNumbers(postSearch.page()), paginationService.sizeWhenGetPageNumbers(postSearch.size()));
+        assertThat(actual).isEqualTo(1L);
     }
 }
