6f096f6f0c3a64f54ba72ccd55d0314a900f7699
youngreal <59333182+youngreal@users.noreply.github.com>
2023-10-06 19:41:13 +0900

    refactor: PostDto record -> class로 변경
    - mybatis에서 원활하게 진행할수있도록 코드를 변경할수도있을것같고, post를 먼저 조회한후 Hashtag를 조회할때 dto의 필드값을 변경해줄 필요가 생겨서 record에서 class로 전환했다.

--- src/main/java/com/example/inflearn/dto/PostDto.java
+++ src/main/java/com/example/inflearn/dto/PostDto.java
@@ -4,21 +4,49 @@ import com.example.inflearn.domain.post.domain.Post;
 import com.example.inflearn.domain.post.domain.PostStatus;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Builder
-public record PostDto(
-        String nickname,
-        String title,
-        String contents,
-        int viewCount,
-        Set<String> hashtags,
-        LocalDateTime createdAt,
-        LocalDateTime updateAt,
-        PostStatus postStatus
-) {
+/*
+querydsl dto projections 에서 기본생성자가 필요했었고 record로는 원활하게 진행되지않아서 DTO로 변환하였다.
+ */
+@ToString
+@Getter
+@NoArgsConstructor
+public class PostDto {
+
+    private Long postId;
+    private String nickname;
+    private String title;
+    private String contents;
+    private int viewCount;
+    private Long likeCount;
+    private Set<String> hashtags;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private PostStatus postStatus;
+
+    @Builder
+    public PostDto(Long postId, String nickname, String title, String contents, int viewCount,
+            Long likeCount, Set<String> hashtags, LocalDateTime createdAt, LocalDateTime updatedAt,
+            PostStatus postStatus) {
+        this.postId = postId;
+        this.nickname = nickname;
+        this.title = title;
+        this.contents = contents;
+        this.viewCount = viewCount;
+        this.likeCount = likeCount;
+        this.hashtags = hashtags;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.postStatus = postStatus;
+    }
 
     public Post toEntity() {
         return Post.builder()
@@ -29,19 +57,12 @@ public record PostDto(
                 .build();
     }
 
-    public static PostDto from(Post post) {
-        return PostDto.builder()
-                .nickname(post.getMember().getNickname())
-                .createdAt(post.getCreatedAt())
-                .updateAt(post.getUpdatedAt())
-                .viewCount(post.getViewCount())
-                .title(post.getTitle())
-                .contents(post.getContents())
-                .hashtags(post.getPostHashtags().stream()
-                        .filter(postHashtag -> postHashtag.getHashtag() != null)
-                        .map(postHashtag -> postHashtag.getHashtag().getHashtagName())
-                        .collect(Collectors.toUnmodifiableSet()))
-                .postStatus(post.getPostStatus())
-                .build();
+    public void setHashtags(List<PostHashtagDto> postHashtagDtos) {
+        if (postHashtagDtos != null) {
+            this.hashtags = postHashtagDtos.stream()
+                    .map(PostHashtagDto::hashtagName)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toUnmodifiableSet());
+        }
     }
 }
