a38c3541ca228fff1a7ebaa6c8f0dad78cbe8d85
youngreal <59333182+youngreal@users.noreply.github.com>
2023-11-04 22:49:19 +0900

    refactor: 엔티티 외래키 제약조건들 제거

--- src/main/java/com/example/inflearn/domain/PostHashtag.java
+++ src/main/java/com/example/inflearn/domain/PostHashtag.java
@@ -3,15 +3,15 @@ package com.example.inflearn.domain;
 import com.example.inflearn.domain.hashtag.domain.Hashtag;
 import com.example.inflearn.domain.post.domain.Post;
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -19,22 +19,27 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+//todo 해당 엔티티는 어떤 패키지에 둬야할까?
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode(of = "id")
 @Getter
+//@Table(indexes = {
+//        @Index(name = "idx_hashtag_post", columnList = "hashtag_id, post_id")
+//})
 @Entity
 public class PostHashtag {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    @JoinColumn(name = "post_id")
     private Post post;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "hashtag_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    @JoinColumn(name = "hashtag_id")
     private Hashtag hashtag;
 
     @Builder
