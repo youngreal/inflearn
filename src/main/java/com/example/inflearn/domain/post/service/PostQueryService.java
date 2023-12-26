b8cac7a771a2e14b918bfbf6069b9a03a897879c
youngreal <59333182+youngreal@users.noreply.github.com>
2023-12-27 00:09:07 +0900

    remove: 불필요한 코드 제거
    * 불필요한 import문, 성능테스트만을 위한 코드 삭제

--- src/main/java/com/example/inflearn/domain/post/service/PostQueryService.java
+++ src/main/java/com/example/inflearn/domain/post/service/PostQueryService.java
@@ -3,12 +3,9 @@ package com.example.inflearn.domain.post.service;
 import static java.util.stream.Collectors.toMap;
 
 import com.example.inflearn.common.exception.DoesNotExistPostException;
-import com.example.inflearn.domain.member.event.Events;
-import com.example.inflearn.domain.member.event.PostViewCountEvent;
 import com.example.inflearn.domain.post.PostDto;
 import com.example.inflearn.domain.post.domain.Post;
 import com.example.inflearn.infra.redis.LikeCountRedisRepository;
-import com.example.inflearn.infra.redis.LikeCountRedisRepository2;
 import com.example.inflearn.infra.repository.dto.projection.PostHashtagDto;
 import com.example.inflearn.infra.mapper.post.PostMapper;
 import com.example.inflearn.infra.repository.post.PostRepository;
@@ -20,7 +17,6 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
@@ -33,126 +29,37 @@ public class PostQueryService {
     private final PostRepository postRepository;
     private final PostMapper postMapper;
     private final LikeCountRedisRepository likeCountRedisRepository;
-    private final LikeCountRedisRepository2 likeCountRedisRepository2;
 
     //todo 게시글 조회와 조회수가 +1 되는 로직은 트랜잭션 분리되어도 될것같은데..? 분리를 고려해보는게 맞을까?
     //todo 테스트 하기도 힘들다
     //todo 좋아요, 댓글을 추가해보고 성능 개선해봐야한다.
-//    @Transactional
-//    public PostDto postDetail(long postId) {
-//        //todo 인기글을 캐싱한다.
-//        Post post = postRepository.findById(postId).orElseThrow(DoesNotExistPostException::new);
-////        post.plusViewCount();
-//        addViewCount(post);
-//        PostDto postDetail = postRepository.postDetail(postId);
-//        postDetail.inputHashtags(postRepository.postHashtagsBy(postDetail));
-//        postDetail.inputComments(postRepository.commentsBy(postDetail));
-//        return postDetail;
-////       return  PostDto.builder()
-////                .title("hi")
-////                .contents("hi")
-////                .nickname("hi")
-////                .build();
-//    }
-
-//    @Transactional
-//    public PostDto postDetail(long postId) {
-//        Post post = postRepository.findById(postId).orElseThrow(DoesNotExistPostException::new);
-//        post.plusViewCount();
-//        PostDto postDetail = postRepository.postDetail(postId);
-//        postDetail.inputHashtags(postRepository.postHashtagsBy(postDetail));
-//        postDetail.inputComments(postRepository.commentsBy(postDetail));
-//        return postDetail;
-//    }
-
-    // v0 레디스 hash사용
-    @Transactional
-    public PostDto postDetailV0(long postId) {
-        Post post = postRepository.findById(postId).orElseThrow(DoesNotExistPostException::new);
-//        Events.raise(new PostViewCountEvent(postId));
-        addViewCount(post);
-        PostDto postDetail = postRepository.postDetail(postId);
-        postDetail.inputHashtags(postRepository.postHashtagsBy(postDetail));
-        postDetail.inputComments(postRepository.commentsBy(postDetail));
-        return postDetail;
-    }
-
     // v1 레디스 hyperloglog사용
     @Transactional
     public PostDto postDetail(long postId) {
+        // 게시글 존재여부 검증
         Post post = postRepository.findById(postId).orElseThrow(DoesNotExistPostException::new);
-//        Events.raise(new PostViewCountEvent(postId));
-        addViewCount2(post);
+
+        // 조회수 업데이트
+        addViewCount(post);
+
+        // 게시글 상세 내용 조회(해시태그, 댓글)
         PostDto postDetail = postRepository.postDetail(postId);
         postDetail.inputHashtags(postRepository.postHashtagsBy(postDetail));
         postDetail.inputComments(postRepository.commentsBy(postDetail));
         return postDetail;
     }
-
-    // v2 레디스 hyperloglog사용 + 조회수 업데이트를 비동기로 처리
-    @Transactional
-    public PostDto postDetailV2(long postId) {
-        Post post = postRepository.findById(postId).orElseThrow(DoesNotExistPostException::new);
-        Events.raise(new PostViewCountEvent(postId));
-//        addViewCount2(post);
-        PostDto postDetail = postRepository.postDetail(postId);
-        postDetail.inputHashtags(postRepository.postHashtagsBy(postDetail));
-        postDetail.inputComments(postRepository.commentsBy(postDetail));
-        return postDetail;
-    }
-
-//    // 범인의심 1 조인쿼리가 범인이다. ( 조인제거)
-//    @Transactional
-//    public PostDto postDetail2(long postId) {
-//        Post post = postRepository.findById(postId).orElseThrow(DoesNotExistPostException::new);
-//        post.plusViewCount();
-//        return PostDto.builder()
-//                .title("내용1")
-//                .contents("제목1")
-//                .nickname("닉네임")
-//                .build();
-//    }
-//
-//    // 범인의심 2 update쿼리가 범인이다. ( 업데이트 제거)
-//    @Transactional
-//    public PostDto postDetail3(long postId) {
-//        Post post = postRepository.findById(postId).orElseThrow(DoesNotExistPostException::new);
-//        PostDto postDetail = postRepository.postDetail(postId);
-//        postDetail.inputHashtags(postRepository.postHashtagsBy(postDetail));
-//        postDetail.inputComments(postRepository.commentsBy(postDetail));
-//        return postDetail;
-//    }
-//
-//    // 범인 의심 3 count쿼리가 범인이다. (postDetail 카운트쿼리)
-//    @Transactional
-//    public PostDto postDetail4(long postId) {
-//        Post post = postRepository.findById(postId).orElseThrow(DoesNotExistPostException::new);
-//        post.plusViewCount();
-//        PostDto postDetail = PostDto.builder()
-//                .postId(2000001L)
-//                .title("내용1")
-//                .contents("제목1")
-//                .nickname("닉네임")
-//                .build();
-//
-//        postDetail.inputHashtags(postRepository.postHashtagsBy(postDetail));
-//        postDetail.inputComments(postRepository.commentsBy(postDetail));
-//        return postDetail;
-//    }
-//
-//    // 범인 의심 4 자바 쿼리가 범인이다?(해시태그, 댓글 입력코드)
-//    @Transactional
-//    public PostDto postDetail5(long postId) {
-//        Post post = postRepository.findById(postId).orElseThrow(DoesNotExistPostException::new);
-//        post.plusViewCount();
-//        PostDto postDetail = postRepository.postDetail(postId);
-////        postDetail.inputHashtags(postRepository.postHashtagsBy(postDetail));
-////        postDetail.inputComments(postRepository.commentsBy(postDetail));
-//        return postDetail;
-//    }
 
     public List<PostDto> searchPost(PostSearch postSearch) {
         List<PostDto> postDtos = postMapper.search(postSearch.searchWord(), paginationService.calculateOffSet(postSearch.page()), postSearch.size(), postSearch.sort());
+        setHashtagsWithJoin(postDtos);
+        return postDtos;
+    }
+
+    /**
+     * 성능테스트 비교를 위한 Like검색쿼리
+     */
+    public List<PostDto> searchPostV2(PostSearch postSearch) {
+        List<PostDto> postDtos = postMapper.searchV2(postSearch.searchWord(), paginationService.calculateOffSet(postSearch.page()), postSearch.size(), postSearch.sort());
         setHashtagsWithJoin(postDtos);
         return postDtos;
     }
@@ -193,8 +100,7 @@ public class PostQueryService {
         Map<Long, Long> popularPosts = getPopularPosts().stream()
                 .collect(toMap(PostDto::getPostId, PostDto::getLikeCount));
 
-//        likeCountRedisRepository.updatePopularPosts(popularPosts);
-        likeCountRedisRepository2.updatePopularPosts(popularPosts);
+        likeCountRedisRepository.updatePopularPosts(popularPosts);
     }
 
     private List<PostDto> getPopularPosts() {
@@ -211,29 +117,15 @@ public class PostQueryService {
         postDtos.forEach(postDto -> postDto.inputHashtags(postHashtagMap.get(postDto.getPostId())));
     }
 
+    // todo 부하테스트 해봐야한다. 레디스는 싱글스레드이기떄문에 조회 요청이 레디스에 엄청몰리면 어느정도 트래픽까집 버틸수있는지.
+    // todo 만약 hyperloglog로 한다면 ? 성능이 어떻게 나올까?
     private void addViewCount(Post post) {
         // validation: 레디스에서 인기글을 가져오고, 레디스에 없다면 DB에서 가져오자
         // 인기글이아니라면(레디스에없다면) 조회수 +1 업데이트, 레디스에있으면 레디스에 조회수 카운팅
         if (likeCountRedisRepository.getViewCount(post.getId()) == null) {
-            log.info("단순 update쿼리 진입");
             post.plusViewCount();
         } else {
-            log.info("updateViewCountToCache 진입");
             likeCountRedisRepository.updateViewCountToCache(post.getId());
-        }
-    }
-
-    // todo 부하테스트 해봐야한다. 레디스는 싱글스레드이기떄문에 조회 요청이 레디스에 엄청몰리면 어느정도 트래픽까집 버틸수있는지.
-    // todo 만약 hyperloglog로 한다면 ? 성능이 어떻게 나올까?
-    private void addViewCount2(Post post) {
-        // validation: 레디스에서 인기글을 가져오고, 레디스에 없다면 DB에서 가져오자
-        // 인기글이아니라면(레디스에없다면) 조회수 +1 업데이트, 레디스에있으면 레디스에 조회수 카운팅
-        if (likeCountRedisRepository2.getViewCount(post.getId()) == null) {
-            log.info("단순 update쿼리 진입");
-            post.plusViewCount();
-        } else {
-            log.info("updateViewCountToCache 진입");
-            likeCountRedisRepository2.updateViewCountToCache(post.getId());
         }
     }
 }
