05366e28d51345136c5256dd2154dab7babb29cd
youngreal <59333182+youngreal@users.noreply.github.com>
2024-01-12 17:09:21 +0900

    remove: 성능테스트용 redis 코드제거
    * UUID 생성, 조회수 +1연산으로 계산했던 테스트용 방식 코드 삭제

--- src/main/java/com/example/inflearn/infra/redis/LikeCountRedisRepository.java
+++ src/main/java/com/example/inflearn/infra/redis/LikeCountRedisRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,22 +24,8 @@ import org.springframework.stereotype.Component;
 public class LikeCountRedisRepository {
 
     private static final String LIKE_COUNT_KEY = "likeCountKey";
-    private final HyperLogLogOperations<Long, String> viewCountOperation; // postId, uniqueString
     private final HyperLogLogOperations<Long, Long> viewCountOperationForTest; // postId, uniqueString
     private final RedisTemplate<String, Long> likeCountOperation; // postId, likeCount
-
-    // for test
-    public Map<Object, Long> getPopularPostEntries2() {
-        log.info("call popularEntries2");
-        Set<Object> keys = likeCountOperation.opsForHash().keys(LIKE_COUNT_KEY);
-        log.info("keys = {}", keys);
-        Map<Object, Long> map = new HashMap<>();
-        for (Object key : keys) {
-            map.put(key, viewCountOperation.size((Long) key));
-        }
-        log.info("map = {}", map);
-        return map;
-    }
 
     public Map<Object, Long> getPopularPostEntries() {
         log.info("call popularEntries");
@@ -55,35 +40,18 @@ public class LikeCountRedisRepository {
     }
 
     public Long getViewCount(Long postId) {
-        return viewCountOperation.size(postId);
-    }
-
-    public Long getViewCount2(Long postId) {
         return viewCountOperationForTest.size(postId);
     }
 
-    // Hash자료구조 처럼 사용하는방식, 메리트가 있을까? 성능테스트는 해보자
-    public void addViewCount(long postId) {
-        viewCountOperationForTest.add(postId, viewCountOperationForTest.size(postId) + 1);
-    }
 
 //    // currentTimeMillis 생성방식 성능테스트 필요
 //    /*
 //     같은 밀리세컨드에 들어온 요청은 중복 카운팅될수있지만 uuid생성비용보단 적을것이다.
 //     */
-    public void addViewCount2(long postId) {
+    public void addViewCount(long postId) {
         log.info("addViewCount2");
         Long value = System.currentTimeMillis();
         viewCountOperationForTest.add(postId, value);
-    }
-//
-//    // uuid 생성방식 성능테스트 필요
-//    /*
-//     조회수 오차가 가장 적을것같은데 uuid 생성비용이 클것으로 예상된다
-//     */
-    public void addViewCount3(long postId) {
-        log.info("addViewCount3");
-        viewCountOperation.add(postId, UUID.randomUUID().toString());
     }
 
     public void updatePopularPosts(Map<Long, Long> popularPostInDB) {
