e60b98797bb9e2db651da1a024e8ca6156f5733c
youngreal <59333182+youngreal@users.noreply.github.com>
2024-02-18 21:35:56 +0900

    refactor: 보조메일 환경변수이름 변경

--- src/test/java/com/example/inflearn/AbstractContainerBaseTest.java
+++ src/test/java/com/example/inflearn/AbstractContainerBaseTest.java
@@ -1,21 +1,21 @@
-package com.example.inflearn;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.containers.GenericContainer;
-
-@SpringBootTest
-public abstract class AbstractContainerBaseTest {
-
-    static final GenericContainer REDIS_CONTAINER;
-
-    static {
-        REDIS_CONTAINER = new GenericContainer<>("redis:7")
-                .withExposedPorts(6379);
-
-        REDIS_CONTAINER.start();
-
-        // 스프링이 레디스와 통신할수있도록 6379와 매핑된 호스트와 포트를 스프링서버에 전달한다.
-        System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
-        System.setProperty("spring.redis.port", String.valueOf(REDIS_CONTAINER.getMappedPort(6379)));
-    }
-}
+//package com.example.inflearn;
+//
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.testcontainers.containers.GenericContainer;
+//
+//@SpringBootTest
+//public abstract class AbstractContainerBaseTest {
+//
+//    static final GenericContainer REDIS_CONTAINER;
+//
+//    static {
+//        REDIS_CONTAINER = new GenericContainer<>("redis:7")
+//                .withExposedPorts(6379);
+//
+//        REDIS_CONTAINER.start();
+//
+//        // 스프링이 레디스와 통신할수있도록 6379와 매핑된 호스트와 포트를 스프링서버에 전달한다.
+//        System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
+//        System.setProperty("spring.redis.port", String.valueOf(REDIS_CONTAINER.getMappedPort(6379)));
+//    }
+//}
