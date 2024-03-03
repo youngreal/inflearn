ae5b630271c91540c8508d35947832f18c492d40
youngreal <59333182+youngreal@users.noreply.github.com>
2024-03-03 23:33:15 +0900

    remove: 더미데이터 생성 코드삭제

--- src/main/java/com/example/inflearn/common/PostHashtagRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.inflearn.common;
-
-import com.example.inflearn.domain.post.domain.PostHashtag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostHashtagRepository extends JpaRepository<PostHashtag, Long> {
-
-}
